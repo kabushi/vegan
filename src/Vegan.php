902fccf809bd7f5089f614749a908fd0a080baca
kelkhali <kelkhali@unil.ch>
2017-12-05 11:07:49 +0100

    vegan first load

---
+++ src/Vegan.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kabushi\Vegan;
+
+/**
+ * Description of Vegan
+ *
+ * @author kelkhali
+ */
+class Vegan
+{
+    public function __construct() {
+        
+    }
+    
+    public function getTitle() {
+        return 'I m vegan.';
+    }
+}
